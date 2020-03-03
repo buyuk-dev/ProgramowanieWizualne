7b5d52cbd738a3a739e77bc63088d355086133ef
buyuk.dev <buyuk.dev@gmail.com>
2020-03-03 22:50:20 +0100

    Add first command binded via viewmodel class.

---
+++ ProgWizApp/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Michalski
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Violin> violins;
+        private ICommand newViolinCmd;
+        public ICommand NewViolinCmd
+        {
+            get {
+                if (newViolinCmd == null) newViolinCmd = new Commands.NewViolinCmd();
+                return newViolinCmd;
+            }
+            set {
+                newViolinCmd = value;
+            }
+        }
+
+        public MainViewModel()
+        {
+            violins = new ObservableCollection<Violin>();
+        }
+    }
+}
