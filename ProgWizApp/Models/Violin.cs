87015d050d4f43d6f5e91584aaee612efca156a3
buyuk.dev <buyuk.dev@gmail.com>
2020-03-04 01:41:26 +0100

    Violin implements INotifyPropertyChanged interface.

--- ProgWizApp/Models/Violin.cs
+++ ProgWizApp/Models/Violin.cs
@@ -15,7 +15,7 @@ namespace Michalski
 		Bad
 	}
 
-	public class Violin
+	public class Violin : INotifyPropertyChanged
 	{
 		public Violin() { }
 		public Violin(String maker, String name, uint year, uint price, string state)
@@ -24,20 +24,77 @@ namespace Michalski
 			this.maker = maker;
 			this.year = year;
 			this.price = price;
-			ViolinState tmpState;
-			Enum.TryParse(state, true, out tmpState);
+			Enum.TryParse(state, true, out ViolinState tmpState);
 			this.state = tmpState;
 		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
 
 		public override string ToString()
 		{
 			return $"{name} made by {maker} in {year} is in {state.ToString()} condition - ${price}";
 		}
 
-		public string name { get; set; }
-		public string maker { get; set; }
-		public uint price { get; set; }
-		public uint year { get; set; }
-		public ViolinState state { get; set; }
+		private string _name;
+		public string name
+		{
+			get { return _name; }
+			set
+			{
+				_name = value;
+				Console.WriteLine($"setting violin name to: {value}");
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("name"));
+			}
+		}
+
+		private string _maker;
+		public string maker
+		{
+			get
+			{
+				return _maker;
+			}
+			set
+			{
+				_maker = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("maker"));
+			}
+		}
+
+		private uint _price;
+		public uint price
+		{
+			get { return _price; }
+			set
+			{
+				_price = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("price"));
+			}
+		}
+
+		private uint _year;
+		public uint year
+		{
+			get
+			{
+				return _year;
+			}
+			set
+			{
+				_year = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("year"));
+			}
+		}
+
+		private ViolinState _state;
+		public ViolinState state
+		{
+			get { return _state; }
+			set
+			{
+				_state = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("state"));
+			}
+		}
 	}
 }
